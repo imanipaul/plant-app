934d1475070711f4a6ffe47678abbec9792e549c
IPaul <ipaul@ny-cou-edit07.turner.com>
2020-02-21 16:32:46 -0500

    Fetch plant info in useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function getResults() {
-  fetch(
-    `http://trefle.io/api/plants/122078?token=dHBJRU9ib1FSSmh5WTJqOGVERkIyZz09`
-  )
-    .then(response => response.json())
-    .then(json => {
-      console.log("result", json);
-    });
-}
-
 function App() {
-  return <div className="App">{getResults()}Plant App</div>;
+  const [plant, setPlant] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `http://trefle.io/api/plants/122078?token=dHBJRU9ib1FSSmh5WTJqOGVERkIyZz09`
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log("result", json);
+        setPlant(json);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      Plant App
+      <div>Plant is</div>
+      {plant && <div>{plant.common_name}</div>}
+    </div>
+  );
 }
 
 export default App;
