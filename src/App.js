bcbfdfed5cea401e3df8b8451a36ee4d29cda550
IPaul <ipaul@ny-cou-edit07.turner.com>
2020-02-21 16:26:09 -0500

    Fetch sample data from trefle.io

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
 
+function getResults() {
+  fetch(
+    `http://trefle.io/api/plants/122078?token=dHBJRU9ib1FSSmh5WTJqOGVERkIyZz09`
+  )
+    .then(response => response.json())
+    .then(json => {
+      console.log("result", json);
+    });
+}
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{getResults()}Plant App</div>;
 }
 
 export default App;
